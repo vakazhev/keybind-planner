668c2fdde471317b991819162b8efb4e98fa3e04
Vakazhev <45141006+rey-av@users.noreply.github.com>
2022-03-30 22:15:40 +0300

    Updates

---
+++ README.md
@@ -0,0 +1,7 @@
+## Keybind Planner
+
+The project to help plan you keybinds for MMO, or anything else.
+
+#### Example
+
+![Example planning](img/example.png)
--- logic.js
+++ logic.js
@@ -1,13 +1,48 @@
-function changeColor(id) {
-    if (document.getElementById(id).style.borderColor == 'red') {
-        document.getElementById(id).style.borderColor = 'yellow';
-    } else if (document.getElementById(id).style.borderColor == 'yellow') {
-        document.getElementById(id).style.borderColor = 'greenyellow';
-    } else if (document.getElementById(id).style.borderColor == 'greenyellow') {
-        document.getElementById(id).style.borderColor = 'aqua';
-    } else if (document.getElementById(id).style.borderColor == 'aqua') {
-        document.getElementById(id).style.borderColor = 'gray';
-    } else {
-        document.getElementById(id).style.borderColor = 'red';
+var defaultColor = 'gray';
+var color = defaultColor;
+
+var textModeEnabled = false;
+
+
+function changeColor(element) {
+    if (textModeEnabled) {
+        var previous = element.textContent;
+
+        var input = prompt('Button name', previous);
+        if (!input) {
+            return;
+        }
+
+        element.textContent = input;
+        return;
     }
+
+    if (element.style.borderColor == color) {
+        element.style.borderColor = defaultColor;
+    } else {
+        element.style.borderColor = color;
+    }
+}
+
+
+function setColor(newColor) {
+    color = newColor;
+    textModeEnabled = false;
+    textModeOff();
+}
+
+
+function resetColor() {
+    color = defaultColor;
+    textModeOff();
+}
+
+
+function textModeOn() {
+    textModeEnabled = true;
+}
+
+
+function textModeOff() {
+    textModeEnabled = false;
 }
